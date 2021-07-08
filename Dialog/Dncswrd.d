097d18b4e14ce080eb4532ac3c2f8f73e306dec6
Aaron Kummer <aaronjkummer@gmail.com>
2021-07-08 01:14:35 -0400

    finally working

---
+++ Dialog/Dncswrd.d
@@ -0,0 +1,12 @@
+BEGIN Dncswrd
+
+
+IF ~True()~ THEN BEGIN 0
+    SAY ~A dancing sword hovers in front of you. It slowly moves closer...~
+        ++ ~Move your hands outward to greet it.~ GOTO 1
+        ++ ~Leave it be.~ EXIT
+END
+
+IF ~~ THEN BEGIN 1
+SAY ~It jumps high into the air then lowers slowly, like a feather into your hands.~ IF ~~ THEN DO ~GiveItemCreate("dncswrd", LastTalkedToBy,0,0,0) DestroySelf()~ EXIT
+END
