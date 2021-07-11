1a352799f8d0c9ed9c8d502c030ff0fb62626f6f
Aaron Kummer <aaronjkummer@gmail.com>
2021-07-11 15:09:20 -0400

    adds a check to see if Elric is in the party, and if he is.. he is gifted Stormbringer

--- Dialog/Dncswrd.d
+++ Dialog/Dncswrd.d
@@ -8,5 +8,13 @@ IF ~True()~ THEN BEGIN 0
 END
 
 IF ~~ THEN BEGIN 1
-SAY ~It jumps high into the air then lowers slowly, like a feather into your hands.~ IF ~~ THEN DO ~GiveItemCreate("dncswrd", LastTalkedToBy,0,0,0) DestroySelf()~ EXIT
+SAY ~It jumps high into the air then lowers slowly, like a feather into your hands.~ 
+    IF ~~ THEN DO ~GiveItemCreate("dncswrd", LastTalkedToBy,0,0,0) DestroySelf()~ EXIT
+    IF ~InParty(Player2) CharName("Elric", Player2)~ GOTO 2 
+END
+
+IF ~~ THEN BEGIN 2
+SAY ~A sense of dread overwhelms you, and a voice calls out... "Elric"
+Suddenly a portal appears and out shoots a black sword. It hovers in front of Elric. He smiles, and the black sword floats into his scabbard.~
+    IF ~~ THEN DO ~GiveItemCreate("STORMBRG", LastTalkedToBy,0,0,0) CreateVisualEffect("SPPORTAL",[2481.3777]) Wait(2) DestroySelf()~ EXIT
 END
